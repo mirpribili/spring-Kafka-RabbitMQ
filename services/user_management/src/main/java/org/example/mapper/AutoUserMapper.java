d3e7c5e60fd14871e891ec0634f466152ea2d879
Bondarev Oleg <bondarev@mail.ru>
2024-03-22 15:49:03 +0300

    code refactor

---
+++ services/user_management/src/main/java/org/example/mapper/AutoUserMapper.java
@@ -0,0 +1,16 @@
+package org.example.springboot.mapper;
+
+import org.example.springboot.dto.UserDto;
+import org.example.springboot.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface AutoUserMapper {
+
+    AutoUserMapper MAPPER = Mappers.getMapper(AutoUserMapper.class);
+
+    UserDto mapToUserDto(User user);
+
+    User mapToUser(UserDto userDto);
+}
