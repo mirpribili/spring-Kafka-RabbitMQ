f4fbbf4dc545c3bd7da204346f6a561a2181cb17
Bondarev Oleg <bondarev@mail.ru>
2024-03-22 15:49:03 +0300

    code refactor

---
+++ services/user_management/src/main/java/org/example/mapper/UserMapper.java
@@ -0,0 +1,29 @@
+package org.example.springboot.mapper;
+
+import org.example.springboot.dto.UserDto;
+import org.example.springboot.entity.User;
+
+public class UserMapper {
+
+    // Convert User JPA Entity into UserDto
+    public static UserDto mapToUserDto(User user){
+        UserDto userDto = new UserDto(
+                user.getId(),
+                user.getFirstName(),
+                user.getLastName(),
+                user.getEmail()
+        );
+        return userDto;
+    }
+
+    // Convert UserDto into User JPA Entity
+    public static User mapToUser(UserDto userDto){
+        User user = new User(
+                userDto.getId(),
+                userDto.getFirstName(),
+                userDto.getLastName(),
+                userDto.getEmail()
+        );
+        return user;
+    }
+}
---
+++ services/user_management/src/main/java/org/example/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package org.example.springboot.repository;
+
+import org.example.springboot.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
