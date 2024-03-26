7b3f7bf97ae6a9cd68aade47b97ab08eac3e5905
Bondarev Oleg <bondarev@mail.ru>
2024-03-26 15:25:20 +0300

    [x] Register EmployeeService Microservice as Eureka Client

--- services/employee-service/src/main/java/net/javaguides/employeeservice/EmployeeServiceApplication.java
+++ services/employee-service/src/main/java/net/javaguides/employeeservice/EmployeeServiceApplication.java
@@ -8,11 +8,13 @@ import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 //I had the same problem when updating the spring-boot version to 3.0.0, I think it's some compatibility bug with spring cloud and spring boot's autoconfigure. I solved it by adding the annotation
 @SpringBootApplication
 @EnableFeignClients
 @ImportAutoConfiguration({FeignAutoConfiguration.class})
+//@EnableEurekaClient
 public class EmployeeServiceApplication {
 
 //	@Bean
