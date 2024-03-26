63885657e4c5000341fc094e18ba8caf809dd79c
Bondarev Oleg <bondarev@mail.ru>
2024-03-26 17:55:36 +0300

    [x] Register API-Gateway as Eureka Client to Eureka Server

--- services/employee-service/src/main/java/net/javaguides/employeeservice/service/APIClient.java
+++ services/employee-service/src/main/java/net/javaguides/employeeservice/service/APIClient.java
@@ -7,7 +7,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url = "http://localhost:8080", value = "DEPARTMENT-SERVICE")
+//@FeignClient(url = "http://localhost:8080", value = "DEPARTMENT-SERVICE")
+@FeignClient(name = "DEPARTMENT-SERVICE")
 public interface APIClient {
     // Build get department rest api
     @GetMapping("api/departments/{department-code}")
